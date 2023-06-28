f61a58604b2039e4010cafa70e261163fce18acd
Williams E. Fernández <williamsenrique@gmail.com>
2023-06-27 20:10:44 -0600

    Personalizando etiquetas de campos desde la clase modelo

--- Data/Student.cs
+++ Data/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolManagementApp.MVC.Data;
 
@@ -7,9 +8,12 @@ public partial class Student
 {
     public int Id { get; set; }
 
+    [Display(Name = "Nombre")]
     public string FirstName { get; set; } = null!;
 
+    [Display(Name = "Apellido")]
     public string LastName { get; set; } = null!;
 
+    [Display(Name = "Fecha de Nacimiento")]
     public DateTime? DateOfBirth { get; set; }
 }
